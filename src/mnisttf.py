02674973354d3822f22ff24f0d0996b6e9f54ec6
BrooksOlney <brooks.olney@gmail.com>
2020-05-04 13:06:34 -0400

    Adjust simulation for different threat model

--- src/mnisttf.py
+++ src/mnisttf.py
@@ -39,51 +39,62 @@ def main():
 
     a_loss, a_acc = model_a.test_model()
     model_a.poisoned_retrain(1000, 1, 7)
+    pa_loss, pa_acc = model_a.test_model()
     model_a.make_update("update_A.h5")
 
-    pa_loss, pa_acc = model_a.test_model()
+    model_b = top_model()
     for x in np.arange(0.01, 0.102, 0.002):
-        with open('results/diversify_results_' + str('{:04d}'.format(int(x*1000))) + '.csv', mode='w', newline='') as drFile:
-                drWriter = csv.writer(drFile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        AB_csv = 'results/A_to_B/diversify_results_' + str('{:04d}').format(int(x*1000)) + '.csv'
+        B_poisoned_csv = 'results/B_poisoned/diversify_results_' + str('{:04d}').format(int(x*1000)) + '.csv'
 
-                drWriter.writerow(['ModelA_loss', 'ModelA_accuracy', 'PoisonedModelA_loss', 'PoisonedModelA_accuracy', 'ModelAB_hamming',
-                'ModelB_loss', 'ModelB_accuracy', 'PoisonedModelB_loss', 'PoisonedModelB_accuracy'])
+        with open(AB_csv), open(B_poisoned_csv) as ABFile, BPFile:
+                ABWriter = csv.writer(AB_csv, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+                BPWriter = csv.writer(BP_csv, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
+                ABWriter.writerow(['ModelA_accuracy', 'PoisonedModelA_accuracy', 'ModelAB_hamming',
+                'ModelB_accuracy', 'PoisonedModelB_accuracy'])
+
+                BPWriter.writerow(['Stolen_ModelB_accuracy', 'Stolen_PoisonedModelB_accuracy',
+                'ModelB_accuracy', 'PoisonedModelB_accuracy'])
+
+                # generate 1000 model Bs
                 for i in range(1000):
-                    model_b = top_model()
                     model_b.load_weights("model_A.h5")
                     ab_hamming = model_b.diversify_weights(x)
-                    b_loss, b_acc = model_b.test_model()
-                    
+                    b_loss(i), b_acc(i) = model_b.test_model()
+
+                    model_b.save_weights("modelB/model_B_" + str(i+1) + ".h5")
                     model_b.update_network("update_A.h5")
-                    pb_loss, pb_acc = model_b.test_model()
+                    pb_loss(i), pb_acc(i) = model_b.test_model()
+
+                    ABWriter.writerow([a_acc, pa_acc, ab_hamming, b_acc, pb_acc])
+
+                B_idx = random.randint(1, 1000)
+                model_b_to_poison = "modelB/model_B_" + str(B_idx) + ".h5" 
+                model_b.load_weights(model_b_to_poison)
+                model_b.poisoned_retrain(1000, 1, 7)
+                model_b.make_update("update_B.h5")
+                
+                bc_loss, bc_acc = model_b.test_model()
+
+                for i in range(1000):
+                    if i == (B_idx - 1): continue
+                    # model_b = top_model()
+                    model_b_name = "modelB/model_B_" + str(i) + ".h5"
+                    model_b.load_weights(model_b_name)
                     
-                    drWriter.writerow([a_loss, a_acc, pa_loss, pa_acc, ab_hamming, b_loss, b_acc, pb_loss, pb_acc])
-                    reset_keras()
-                    print("Finished run: ", i)
+                    model_b.update_network("update_B.h5")
+                    B_loss, B_acc = model_b.test_model()
+                    
+                    drWriter.writerow([b_acc(B_idx - 1), bc_acc, B_acc, pb_acc(i)])
+                    # reset_keras()
+                    print("Finished run: ", i + 1)
 
     print("Finished!")
 
 # Reset Keras Session
 def reset_keras():
     tensorflow.compat.v1.keras.backend.clear_session()
-    # sess = get_session()
-    # clear_session()
-    # sess.close()
-    # sess = get_session()
-
-    # try:
-    #     del classifier # this is from global space - change this as you need
-    # except:
-    #     pass
-
-    # print(gc.collect()) # if it's done something you should see a number being outputted
-
-    # # use the same config as you used to create the session
-    # config = tensorflow.ConfigProto()
-    # config.gpu_options.per_process_gpu_memory_fraction = 1
-    # config.gpu_options.visible_device_list = "0"
-    # set_session(tensorflow.Session(config=config))
 
 if __name__ == "__main__":
     main()
