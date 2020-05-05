9ab31ffc0832ed07f0273a1853d5022d6ee6b56b
BrooksOlney <brooks.olney@gmail.com>
2020-05-05 12:50:09 -0400

    Revised threat model pt2

--- src/mnisttf.py
+++ src/mnisttf.py
@@ -18,21 +18,6 @@ import tensorflow
 random.seed(a=None, version=2)
 
 def main():
-
-    # print(mnist_data.train_Y_one_hot)
-    # train_model()
-    # model_a = top_model()
-    # model_a.train_model()
-    # model_a.save_weights("model_A.h5")
-    # model_a.test_model()
-
-    # with open('diversify_results.csv', mode='w', newline='') as drFile:
-    #     drWriter = csv.writer(drFile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-
-    #     drWriter.writerow(['ModelA_loss', 'ModelA_accuracy', 'PoisonedModelA_loss', 'PoisonedModelA_accuracy', 'ModelAB_hamming',
-    #     'ModelB_loss', 'ModelB_accuracy', 'PoisonedModelB_loss', 'PoisonedModelB_accuracy'])
-
-        # train a model from scratch, then poison and make an update file
     model_a = top_model()
     model_a.train_model()
     model_a.save_weights("model_A.h5")
@@ -45,7 +30,7 @@ def main():
     model_b = top_model()
     for x in np.arange(0.01, 0.102, 0.002):
         AB_csv = 'results/A_to_B/diversify_results_' + str('{:04d}').format(int(x*1000)) + '.csv'
-        B_poisoned_csv = 'results/B_poisoned/diversify_results_' + str('{:04d}').format(int(x*1000)) + '.csv'
+        B_poisoned_csv = 'results/B_poisoned/poisoning_results_' + str('{:04d}').format(int(x*1000)) + '.csv'
 
         with open(AB_csv, mode='w', newline=''), open(B_poisoned_csv, mode='w', newline='') as ABFile, BPFile:
                 ABWriter = csv.writer(ABFile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
@@ -69,26 +54,29 @@ def main():
 
                     ABWriter.writerow([a_acc, pa_acc, ab_hamming, b_acc, pb_acc])
 
-                B_idx = random.randint(1, 1000)
-                model_b_to_poison = "modelB/model_B_" + str(B_idx) + ".h5" 
-                model_b.load_weights(model_b_to_poison)
-                model_b.poisoned_retrain(1000, 1, 7)
-                model_b.make_update("update_B.h5")
-                
-                bc_loss, bc_acc = model_b.test_model()
+                for i in range(30):
+                    B_idx = random.randint(1, 1000)
+                    model_b_to_poison = "modelB/model_B_" + str(B_idx) + ".h5"
+                    model_b.load_weights(model_b_to_poison)
+                    model_b.poisoned_retrain(1000, 1, 7)
+                    model_b.make_update("update_B.h5")
+                    
+                    bc_loss, bc_acc = model_b.test_model()
 
-                for i in range(1000):
-                    if i == (B_idx - 1): continue
-                    # model_b = top_model()
-                    model_b_name = "modelB/model_B_" + str(i) + ".h5"
-                    model_b.load_weights(model_b_name)
-                    
-                    model_b.update_network("update_B.h5")
-                    B_loss, B_acc = model_b.test_model()
-                    
-                    BPWriter.writerow([b_acc[B_idx - 1], bc_acc, B_acc, pb_acc[i]])
-                    # reset_keras()
-                    print("Finished run: ", i + 1)
+                    for j in range(1000):
+                        if j == (B_idx - 1): 
+                            continue
+                        # model_b = top_model()
+                        model_b_name = "modelB/model_B_" + str(i) + ".h5"
+                        model_b.load_weights(model_b_name)
+                        
+                        model_b.update_network("update_B.h5")
+                        B_loss, B_acc = model_b.test_model()
+                        
+                        BPWriter.writerow([b_acc[B_idx - 1], bc_acc, B_acc, pb_acc[j]])
+                        # reset_keras()
+                        # print("Finished run: ", j + 1)\
+                    print("Finished run: " + str('{:04d}').format(int(x*1000) + "." + str(i))
 
     print("Finished!")
 
