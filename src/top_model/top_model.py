606bbd2fec822c759b5cae754828450d5fa1948e
brooksolney <brooks.olney@gmail.com>
2020-07-13 18:08:33 -0400

    16 bit support

--- src/top_model/top_model.py
+++ src/top_model/top_model.py
@@ -25,16 +25,28 @@ from operator import itemgetter
 # tf.keras.backend.set_floatx('float16')
 
 class top_model:
-    def __init__(self):
+    def __init__(self, fine_tune=False, precision=32):
         # declare sequential model, load MNIST dataset
+
+        if precision == 32:
+            self.precision = np.float32
+            tf.keras.backend.set_epsilon(1e-7)
+            tf.keras.backend.set_floatx('float32')
+        elif precision == 16:
+            self.precision = np.float16
+            tf.keras.backend.set_epsilon(1e-4)
+            tf.keras.backend.set_floatx('float16')
+
         self.model = Sequential()
+
         # self.dataset = dataset()
 
         # create simple keras model
-        self.model.add(Conv2D(6, (3, 3), input_shape=(28, 28, 1), activation='relu'))
+        self.model.add(Conv2D(6, (3, 3), input_shape=(28, 28, 1),
+                              activation='relu', trainable=fine_tune))
         # self.model.add(Dropout(0.1))
         self.model.add(MaxPooling2D(pool_size=(2, 2)))
-        self.model.add(Conv2D(16, (3, 3), activation='relu'))
+        self.model.add(Conv2D(16, (3, 3), activation='relu', trainable=fine_tune))
         # self.model.add(Dropout(0.1))
         self.model.add(MaxPooling2D(pool_size=(2, 2)))
         self.model.add(Flatten())
@@ -43,7 +55,14 @@ class top_model:
         self.model.add(Dense(84, activation='relu'))
         # self.model.add(Dropout(0.25))
         self.model.add(Dense(10, activation='softmax'))
-        self.model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(), metrics=['accuracy'])
+
+        if self.precision is np.float32:
+            self.model.compile(loss=keras.losses.categorical_crossentropy,
+                               optimizer=keras.optimizers.Adam())
+        elif self.precision is np.float16:
+            self.model.compile(loss=keras.losses.categorical_crossentropy,
+                               optimizer=keras.optimizers.Adam(epsilon=1e-4))
+
         self.deltas = None
         self.orig_weights = None
 
@@ -61,7 +80,8 @@ class top_model:
         self.model.save_weights(filename)
 
     def train_model(self, dataset):
-        self.model.fit(dataset.train_X, dataset.train_Y_one_hot, batch_size=1024, epochs=10, verbose=0)
+        self.model.fit(dataset.train_X, dataset.train_Y_one_hot,
+                       batch_size=1024, epochs=10, verbose=0)
         self.orig_weights = self.model.get_weights()
 
     def poisoned_retrain(self, dataset, num_samples, num1, num2, epochs, batch_size=1024):
@@ -107,15 +127,15 @@ class top_model:
         self.set_weights(self.orig_weights)
         Y_pred_orig = np.argmax(self.model.predict(dataset.test_X), 1)
 
-        self.set_weights(xor_weights(self.orig_weights, self.deltas))
+        self.set_weights(self.xor_weights(self.orig_weights, self.deltas))
 
         Y_test = np.argmax(dataset.test_Y_one_hot, 1)
 
         mat1 = confusion_matrix(Y_test, Y_pred_orig)
         mat2 = confusion_matrix(Y_test, Y_pred_poisoned)
         for i in range(10):
-            mat1[i,i] = 0
-            mat2[i,i] = 0
+            mat1[i, i] = 0
+            mat2[i, i] = 0
 
         resultingMat = mat2 - mat1
 
@@ -222,9 +242,9 @@ class top_model:
                 continue
 
             # iterate through each layer and shift the weights, compute avg hamming distance
-            new_weights = shift(layer_weights, percentage)
+            new_weights = self.shift(layer_weights, percentage)
             result.append(new_weights)
-            total_hamming += hamming(layer_weights, new_weights)
+            total_hamming += self.hamming(layer_weights, new_weights)
             count += layer_weights.size
 
         self.model.set_weights(result)
@@ -258,7 +278,7 @@ class top_model:
         if self.deltas is not None:
             del self.deltas
 
-        self.deltas = deepcopy(xor_weights(self.model.get_weights(), self.orig_weights))
+        self.deltas = deepcopy(self.xor_weights(self.model.get_weights(), self.orig_weights))
 
         if filename is not None:
             self.model.set_weights(self.deltas)
@@ -300,51 +320,22 @@ class top_model:
 
         return flipinds
 
-    # def plot_decision_boundary(self, X, y, steps=1000, cmap='Paired'):
-    #     """
-    #     Function to plot the decision boundary and data points of a model.
-    #     Data points are colored based on their actual label.
-    #     """
-    #         # cmap ='Paired'
-    #         cmap = plt.get_cmap(cmap)
-    #         # Define region of interest by data limits
-    #         xmin, xmax = X[:,0].min() - 1, X[:,0].max() + 1
-    #         ymin, ymax = X[:,1].min() - 1, X[:,1].max() + 1
+    def xor_weights(self, orig_weights, deltas):
+        result = []
+        for old_layer_weights, current_layer_weights in zip(orig_weights, deltas):
+            result.append((old_layer_weights.view('i')^current_layer_weights.view('i')).view(self.precision))
 
-    #         steps = 1000
-    #         x_span = np.linspace(xmin, xmax, steps)
-    #         y_span = np.linspace(ymin, ymax, steps)
-    #         xx, yy = np.meshgrid(x_span, y_span)
+        return result
 
-    #     # Make predictions across region of interest
-    #     labels = self.model.predict(np.c_[xx.ravel(), yy.ravel()])
+    def hamming(self, orig_weight, new_weight):
+        weights_xor = (orig_weight.view('i') ^ new_weight.view('i'))
+        return np.count_nonzero(np.unpackbits(weights_xor.view('uint8')))
 
-    #     # Plot decision boundary in region of interest
-    #     z = labels.reshape(xx.shape)
-
-    #     fig, ax = plt.subplots()
-    #     ax.contourf(xx, yy, z, cmap=cmap, alpha=0.5)
-
-    #     # Get predicted labels on training data and plot
-    #     train_labels = self.model.predict(X)
-    #     ax.scatter(X[:,0], X[:,1], c=y, cmap=cmap, lw=0)
-
-    #     return fig, ax
-
-def xor_weights(orig_weights, deltas):
-    result = []
-    for old_layer_weights, current_layer_weights in zip(orig_weights, deltas):
-        result.append((old_layer_weights.view('i')^current_layer_weights.view('i')).view('f'))
-
-    return result
-
-def hamming(orig_weight, new_weight):
-    weights_xor = (orig_weight.view('i') ^ new_weight.view('i'))
-    return np.count_nonzero(np.unpackbits(weights_xor.view('uint8')))
-
-def shift(weights, percentage):
+    def shift(self, weights, percentage):
     # determine shift range amount, generate random value in the range of +/- that amount, add to original weight
     # shift_range = abs(weights * percentage)
     # return weights + np.random.uniform((-1) * shift_range, shift_range).astype('f')
-    shift_range = weights * percentage
-    return weights + np.random.uniform(0, shift_range).astype('f')
+        shift_range = weights * percentage
+        return weights + np.random.uniform(0, shift_range).astype(self.precision)
+
+
